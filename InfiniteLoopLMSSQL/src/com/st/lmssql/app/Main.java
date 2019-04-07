3d0da4394348a133e877c152c42d81bc81817ebb
vyeung <vl.yeung@yahoo.com>
2019-04-07 16:30:47 -0400

    add menu call to Main

--- InfiniteLoopLMSSQL/src/com/st/lmssql/app/Main.java
+++ InfiniteLoopLMSSQL/src/com/st/lmssql/app/Main.java
@@ -1,10 +1,13 @@
 package com.st.lmssql.app;
 
+import com.st.lmssql.menus.Menu;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		System.out.println("Welcome to the Smoothstack Library Management System. Which category of user are you? \n");
+		Menu m = new Menu();
+		m.mainMenu();		
 	}
-
 }
